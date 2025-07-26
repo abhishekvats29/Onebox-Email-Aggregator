63080d90116a24ba7daa21c8e93964ce2317bc70
Abhishek Vats <161937254+abhishekvats29@users.noreply.github.com>
2025-07-26 18:23:57 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://onebox-backend-apx8.onrender.com/',
+        target: 'http://localhost:3000/api/emails',
         changeOrigin: true,
         secure: false,
       },
