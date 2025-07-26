3a48f42a6787a36c8ff7c61972e531ba789ca27a
Abhishek Vats <161937254+abhishekvats29@users.noreply.github.com>
2025-07-26 17:37:39 +0530

    Update emailService.ts

--- frontend/src/services/emailService.ts
+++ frontend/src/services/emailService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Email } from "../types/email.ts";
 
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://onebox-backend-apx8.onrender.com";
 
 export const getEmails = async (): Promise<Email[]> => {
   const response = await axios.get(`${BASE_URL}/emails`);
